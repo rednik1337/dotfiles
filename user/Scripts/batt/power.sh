4d11e0137f60c2cbe16b1e3c5efba92a91117de4
rednik1337 <micalculator@bk.ru>
2024-09-01 22:33:11 +0300

    upd

--- user/Scripts/batt/power.sh
+++ user/Scripts/batt/power.sh
@@ -2,5 +2,5 @@
 
 cpupower frequency-set -g powersave
 echo "power" | tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference
-echo power > /sys/module/pcie_aspm/parameters/policy
+echo powersupersave > /sys/module/pcie_aspm/parameters/policy
 
